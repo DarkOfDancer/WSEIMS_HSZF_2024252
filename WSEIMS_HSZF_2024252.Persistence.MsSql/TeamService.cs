4e99a5bff929a04ae9bfe1aef6118faf2bdb800b
Molnár Zsófia <darkofdancer@stud.uni-obuda.hu>
2025-04-11 12:23:25 +0200

    Szűrések hozzáadva

---
+++ WSEIMS_HSZF_2024252.Persistence.MsSql/TeamService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSEIMS_HSZF_2024252.Model;
+
+namespace WSEIMS_HSZF_2024252.Persistence.MsSql
+{
+    public class TeamService
+    {
+        public List<TeamEntity> GetTeamsPaged(int pageNumber, int pageSize)
+        {
+            using var context = new FormulaOneDbContext();
+
+            return context.Teams
+                .OrderBy(t => t.year)
+                .ThenBy(t => t.teamName)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+    }
+
+}
--- WSEIMS_HSZF_2024252/Program.cs
+++ WSEIMS_HSZF_2024252/Program.cs
@@ -1,27 +1,44 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 using System;
 using WSEIMS_HSZF_2024252.Model;
 using WSEIMS_HSZF_2024252.Persistence.MsSql;
+
 
 namespace WSEIMS_HSZF_2024252
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Menu(JsonImporter jsonImporter)
         {
-            var ctx = new FormulaOneDbContext();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("===== Formula One Budget Console App =====");
+                Console.WriteLine("1. Csapatok listázása (lapozás)");
+                Console.WriteLine("2. Keresés");
+                Console.WriteLine("3. Csapat frissítése");
+                Console.WriteLine("4. Csapat törlése");
+                Console.WriteLine("5. Fájl importálása");
+                Console.WriteLine("6. Kilépés");
+                Console.Write("Válassz egy opciót: ");
 
-            string rootDirectory = @"C:\Users\zsofi\source\repos\WSEIMS_HSZF_2024252\WSEIMS_HSZF_2024252.Model";  // A gyökér könyvtár
+                var input = Console.ReadLine();
+                Console.Clear();
+                
+                
+            }
+        }
 
-            // Az importáló osztály példányosítása
-            var jsonImporter = new JsonImporter();
-
+        
+        static void StartUpload(string rootDirectory,JsonImporter jsonImporter)
+        {
             // JSON fájlok importálása és eredmény kiírása
             var resultMessage = jsonImporter.ImportTeamsFromJsonDirectory(rootDirectory);
 
             // Az importálás eredményének kiírása
 
-           if (resultMessage == null)
+            if (resultMessage == null)
             {
                 Console.WriteLine("A könyvtár nem található, vagy egy hiba történt.");
             }
@@ -29,6 +46,19 @@ namespace WSEIMS_HSZF_2024252
             {
                 Console.WriteLine($"Sikeresen importálva {resultMessage.Count} csapat.");
             }
+
+        }
+        static void Main(string[] args)
+        {
+            var ctx = new FormulaOneDbContext();
+            // A gyökér könyvtár
+            string rootDirectory = @"C:\Users\zsofi\source\repos\WSEIMS_HSZF_2024252\WSEIMS_HSZF_2024252.Model";  
+            // Az importáló osztály példányosítása
+            var jsonImporter = new JsonImporter();
+            StartUpload(rootDirectory,jsonImporter);
+            Menu(jsonImporter);
+            var service = new TeamService();
+            var result = service.SearchTeams(field, value, exact);
         }
     }
 }
